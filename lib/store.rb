3d8367b4eb289a35ecdebde1721f5aae57bb7b2d
Johnny <johnnylabedzki@hotmail.com>
2021-10-11 15:57:45 -0700

    Complete exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -1,9 +1,11 @@
 class Store < ActiveRecord::Base
   has_many :employees
-  
+
   validates :name, presence: true, length: {minimum: 3}
   validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
   validate :must_carry_clothing
+
+  before_destroy :confirm_zero_employees
 
   def must_carry_clothing
     if !mens_apparel && !womens_apparel
@@ -12,5 +14,13 @@ class Store < ActiveRecord::Base
       errors.add(:womens_apparel, "or mens apparel must be in your store.")
     end
   end
+
+  private
+
+  def confirm_zero_employees
+    if self.employees.count > 0
+      return false
+    end
+  end 
 
 end
